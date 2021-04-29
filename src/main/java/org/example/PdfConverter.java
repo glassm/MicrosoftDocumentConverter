1875087f8634bb0273d1dddac5c99d62f9b07ea8
mglass <mark.glass@cgifederal.com>
2021-04-29 17:07:11 -0400

    Fix some Doc(x) to PDF issues

--- src/main/java/org/example/PdfConverter.java
+++ src/main/java/org/example/PdfConverter.java
@@ -12,12 +12,15 @@ import com.itextpdf.html2pdf.HtmlConverter;
 import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import org.apache.commons.codec.CharEncoding;
 
 public class PdfConverter {
 
     public static void main(String[] args) throws IOException {
         xlsDemo(new FileInputStream("example/test.xls"), new FileOutputStream("example/out/test_xls.pdf"));
         xlsxDemo(new FileInputStream("example/test.xlsx"), new FileOutputStream("example/out/test_xlsx.pdf"));
+        docDemo(new FileInputStream("example/resume.doc"), new FileOutputStream("example/out/resume_doc.pdf"));
+        docxDemo(new FileInputStream("example/resume.docx"), new FileOutputStream("example/out/resume_docx.pdf"));
 
 
     }
@@ -27,10 +30,13 @@ public class PdfConverter {
 
 
         XlsConverter xlsConverter = new XlsConverter(is);
-        xlsConverter.convertToHtmlStream(baos,  "UTF-8"); //closes input stream automatically
+        xlsConverter.convertToHtmlStream(baos,  CharEncoding.UTF_8); //closes input stream automatically
 
         //create a pdf document and set it's page size
-        writeStreamToPdf(is, os, baos);
+        writeStreamToPdf(baos, os);
+
+        is.close();
+        os.close();
     }
 
     private static void xlsxDemo(InputStream is, OutputStream os) throws IOException {
@@ -38,11 +44,36 @@ public class PdfConverter {
         XlsxConverter converter = new XlsxConverter(is);
 
         converter.convertToHtmlStream(baos);
-        writeStreamToPdf(is, os, baos);
+        writeStreamToPdf(baos, os);
 
+        is.close();
+        os.close();
     }
 
-    private static void writeStreamToPdf(InputStream is, OutputStream os, ByteArrayOutputStream baos) throws IOException {
+    private static void docDemo(InputStream is, OutputStream os) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        DocConverter converter = new DocConverter(is);
+        converter.convertToHtmlStream(baos,  CharEncoding.UTF_8);
+
+        writeStreamToPdf(baos, os);
+
+        is.close();
+        os.close();
+    }
+
+    private static void docxDemo(InputStream is, OutputStream os) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DocxConverter converter = new DocxConverter(is);
+
+        converter.convertToHtmlStream(baos, CharEncoding.UTF_8);
+        writeStreamToPdf(baos, os);
+
+        is.close();
+        os.close();
+    }
+
+    private static void writeStreamToPdf(ByteArrayOutputStream baos, OutputStream os) throws IOException {
         //create a pdf document and set it's page size
         PdfDocument doc = new PdfDocument(new PdfWriter(os));
         doc.setDefaultPageSize(new PageSize(3000, 2000));
@@ -52,9 +83,6 @@ public class PdfConverter {
         ConverterProperties props = new ConverterProperties();
         HtmlConverter.convertToPdf(String.valueOf(baos), doc, props);
 
-        baos.close();
         doc.close();
-        is.close();
-        os.close();
     }
 }
